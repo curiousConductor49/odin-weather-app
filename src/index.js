1ad58e1aabe631673dde1c2312f0a4f98054e610
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-08-03 18:23:35 -0400

    chore: pseudocode general program functionality

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/* 
+PSEUDOCODE
+
+top-level:
+module imports (api-data, data-display, user-input)
+
+event listener scope:
+- user btn click -> anon async callback wrapping the bulk of the program
+    - await fetch weather API data, both general and celsius temp (async)
+    - set DOM display visibility from hidden to visible
+    - display weather API data (async)
+    - attach event listener to temp toggle checkbox
+        - checks if checkbox is checked or unchecked
+        - if checked, display celsius temp data
+        - else, display fahrenheit temp data
+    - attach event listener to error msg dialog
+        - close dialog when close btn is clicked
+*/
