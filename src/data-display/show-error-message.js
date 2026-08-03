5d2e50ae404399aa8ea92cbe58a50926890c05f7
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-08-03 18:14:04 -0400

    chore: pseudocode functionality to display weather data

---
+++ src/data-display/show-error-message.js
@@ -0,0 +1,10 @@
+/* 
+PSEUDOCODE
+
+function show error msg
+Params: error msg dialog
+Body:
+- create an appropriate error message
+- set the error msg as the body text of the dialog
+Return: n/a
+*/
---
+++ src/data-display/show-temp-data.js
@@ -0,0 +1,8 @@
+/* 
+PSEUDOCODE
+
+function show temp data
+Params: temp data (obj), data display containers (arr of DOM elements)
+Body:
+- set the properties of the js object (only temp data in Celsius) to be the text content of their respective DOM elements
+*/
