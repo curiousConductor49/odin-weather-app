c5215927c087876e214261b4a94acf581dfd9356
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-08-03 17:03:02 -0400

    chore: pseudocode function to clean and return user input

---
+++ src/user-input/clean-user-input.js
@@ -0,0 +1,9 @@
+/* 
+PSEUDOCODE:
+
+Function clean user input
+Params: user input val (text string)
+Body:
+- store whitespace-trimmed, lowercased val in variable i.e. clean it
+Return: cleaned user input val
+*/
