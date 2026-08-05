52f30822adad7387db52aba86f02954f9324cfb1
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-08-05 13:28:40 -0400

    feat: create function to clean and return user input

--- src/user-input/clean-user-input.js
+++ src/user-input/clean-user-input.js
@@ -7,3 +7,9 @@ Body:
 - store whitespace-trimmed, lowercased val in variable i.e. clean it
 Return: cleaned user input val
 */
+
+export function cleanUserInput(input) {
+    const cleanInput = input.trim().toLowerCase();
+
+    return cleanInput;
+}
