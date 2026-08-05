e8c0911577267e84252e3ad2a572ec4118e8d580
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-08-05 15:27:36 -0400

    chore: adjust pseudocode for slimmed weather data properties

--- src/api-data/weather-data.js
+++ src/api-data/weather-data.js
@@ -18,10 +18,10 @@ Methods:
     - accepts the resolved API data (JSON) as arg
     - converts the API data into a js obj
     - create and return a js object w/ only the necessary data
-        - temperature (tempmax, tempmin, temp, feelslike)
+        - temperature (temp, feelslike)
         - humidity
         - windspeed
-        - description
+        - conditions
         - icon
 */
 
