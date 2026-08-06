12acb0bd0d1580b59106706c7b78f8d3228b7d8e
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-08-06 14:54:00 -0400

    chore: rename function import

--- src/data-display/show-general-data.js
+++
@@ -1,8 +0,0 @@
-/* 
-PSEUDOCODE
-
-function show general data
-Params: weather data (obj), data display containers (arr of DOM elements)
-Body:
-- set the properties of the js object to be the text content of their respective DOM elements (including the img src for the weather icon)
-*/
