92d7f3abea2d6463ec543ed8f1558c972b976d94
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-08-06 18:01:07 -0400

    feat: create function to display the temperature data

--- src/data-display/show-temp-data.js
+++ src/data-display/show-temp-data.js
@@ -6,3 +6,15 @@ Params: temp data (obj), data display containers (arr of DOM elements)
 Body:
 - set the properties of the js object (only temp data in Celsius) to be the text content of their respective DOM elements
 */
+
+export function showTempData(data, displays, isUnitMetric) {
+    try {
+        const { temp, feelslike } = data;
+        const [ , , , , tempDisplay, feelslikeDisplay ] = displays;
+
+        tempDisplay.textContent = `${temp} ${isUnitMetric ? "C°" : "F°"}`;
+        feelslikeDisplay.textContent = `Feels ${feelslike} ${isUnitMetric ? "C°" : "F°"}`;
+    } catch (error) {
+        console.log("Error:", error.message);
+    }
+}
