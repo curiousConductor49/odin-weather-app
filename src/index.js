5ec218c0d2f7af5cdff8952f005c0c3fc02190b7
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-08-06 18:11:29 -0400

    feat: implement toggle for celsius vs fahrenheit weather data

--- src/index.js
+++ src/index.js
@@ -20,11 +20,13 @@ event listener scope:
 import { cleanUserInput } from "./user-input/clean-user-input.js";
 import { WeatherData } from "./api-data/weather-data.js";
 import { showStandardData } from "./data-display/show-standard-data.js";
+import { showTempData } from "./data-display/show-temp-data.js";
 
 
 // TESTING ZONE
 const weatherSearchForm = document.querySelector("#search-form");
 const weatherSearchBar = document.querySelector("#weather-search");
+const tempUnitToggle = document.querySelector("#temp-unit-toggle");
 
 const conditionsDisplay = document.querySelector("#conditions-display");
 const humidityDisplay = document.querySelector("#humidity-display");
@@ -37,20 +39,28 @@ const weatherIconDisplay = document.querySelector("#weather-icon-display");
 weatherSearchForm.addEventListener("submit", async (event) => {
     try {
         event.preventDefault();
+
         const userInput = event.target.querySelector("#weather-search").value;
+
         const weather = new WeatherData(userInput);
         const apiResults = await weather.queryStandardData();
-        const apiTempResults = await weather.queryTempCelsiusData();
+        const apiTempCelsiusResults = await weather.queryTempCelsiusData();
+        const apiFahrenheitResults = await weather.queryTempFahrenheitData();
+
         const standard = weather.processStandardData(apiResults);
-        const temp = weather.processTempCelsiusData(apiTempResults);
+        const tempCelsius = weather.processTempData(apiTempCelsiusResults);
+        const tempFarenheit = weather.processTempData(apiFahrenheitResults);
+
         const displays = [conditionsDisplay, humidityDisplay, windspeedDisplay, windgustDisplay, tempDisplay, feelslikeDisplay, weatherIconDisplay];
 
-        console.log(apiTempResults);
         console.log(apiResults);
         console.log(standard);
-        console.log(temp);
+        console.log(tempCelsius);
+        console.log(tempFarenheit);
 
         showStandardData(standard, displays);
+
+        tempUnitToggle.addEventListener("change", (event) => event.target.checked ? showTempData(tempCelsius, displays, true) : showTempData(tempFarenheit, displays, false));
     } catch (error) {
         console.log("Error:", error.message);
     }
