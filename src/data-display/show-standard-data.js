a18601023b1d027895273545c718890d869e8251
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-08-06 17:19:38 -0400

    feat: update function from sync to async to utilize dynamic imports for weather data icons

--- src/data-display/show-standard-data.js
+++ src/data-display/show-standard-data.js
@@ -7,15 +7,22 @@ Body:
 - set the properties of the js object to be the text content of their respective DOM elements (including the img src for the weather icon)
 */
 
-export function showStandardData(data, displays) {
-    const { conditions, humidity, windspeed, windgust, temp, feelslike, icon } = data;
-    const [ conditionsDisplay, humidityDisplay, windspeedDisplay, windgustDisplay, tempDisplay, feelslikeDisplay, weatherIconDisplay ] = displays;
 
-    conditionsDisplay.textContent = conditions;
-    humidityDisplay.textContent = `Humidity: ${humidity}%`;
-    windspeedDisplay.textContent = `Wind: ${windspeed} mph`;
-    windgustDisplay.textContent = `Gust: ${windgust} mph`;
-    tempDisplay.textContent = `${temp} F°`;
-    feelslikeDisplay.textContent = `Feels ${feelslike} F°`;
-    weatherIconDisplay.src = `src/assets/icons/${icon}.svg`;
+export async function showStandardData(data, displays) {
+    try {
+        const { conditions, humidity, windspeed, windgust, temp, feelslike, icon } = data;
+        const [ conditionsDisplay, humidityDisplay, windspeedDisplay, windgustDisplay, tempDisplay, feelslikeDisplay, weatherIconDisplay ] = displays;
+        const svgModule = await import(`../assets/${icon}.svg`);
+
+        conditionsDisplay.textContent = conditions;
+        humidityDisplay.textContent = `Humidity: ${humidity}%`;
+        windspeedDisplay.textContent = `Wind: ${windspeed} mph`;
+        windgustDisplay.textContent = `Gust: ${windgust} mph`;
+        tempDisplay.textContent = `${temp} F°`;
+        feelslikeDisplay.textContent = `Feels ${feelslike} F°`;
+        weatherIconDisplay.src = svgModule.default;
+        weatherIconDisplay.alt = conditions.toLowerCase();
+    } catch (error) {
+        console.log("Error:", error.message);
+    }
 }
