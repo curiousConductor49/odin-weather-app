daf0f3c06a0ef98750e9d33397ef78d50ef6c086
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-08-06 15:27:31 -0400

    chore: test weather data display functionality

--- src/index.js
+++ src/index.js
@@ -19,23 +19,35 @@ event listener scope:
 
 import { cleanUserInput } from "./user-input/clean-user-input.js";
 import { WeatherData } from "./api-data/weather-data.js";
+import { showStandardData } from "./data-display/show-standard-data.js";
 
 
 // TESTING ZONE
-// const weatherSearchForm = document.querySelector("#search-form");
-// const weatherSearchBar = document.querySelector("#weather-search");
+const weatherSearchForm = document.querySelector("#search-form");
+const weatherSearchBar = document.querySelector("#weather-search");
 
-// weatherSearchForm.addEventListener("submit", async (event) => {
-//     event.preventDefault();
-//     const userInput = event.target.querySelector("#weather-search").value;
-//     const weather = new WeatherData(userInput);
-//     const apiResults = await weather.queryStandardData();
-//     const apiTempResults = await weather.queryTempCelsiusData();
-//     const standard = weather.processStandardData(apiResults);
-//     const temp = weather.processTempCelsiusData(apiTempResults);
+const conditionsDisplay = document.querySelector("#conditions-display");
+const humidityDisplay = document.querySelector("#humidity-display");
+const windspeedDisplay = document.querySelector("#windspeed-display");
+const windgustDisplay = document.querySelector("#windgust-display");
+const tempDisplay = document.querySelector("#temp-display");
+const feelslikeDisplay = document.querySelector("#feelslike-display");
+const weatherIconDisplay = document.querySelector("#weather-icon-display");
 
-//     console.log(apiTempResults);
-//     console.log(apiResults);
-//     console.log(standard);
-//     console.log(temp);
-// })
+weatherSearchForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const userInput = event.target.querySelector("#weather-search").value;
+    const weather = new WeatherData(userInput);
+    const apiResults = await weather.queryStandardData();
+    const apiTempResults = await weather.queryTempCelsiusData();
+    const standard = weather.processStandardData(apiResults);
+    const temp = weather.processTempCelsiusData(apiTempResults);
+    const displays = [conditionsDisplay, humidityDisplay, windspeedDisplay, windgustDisplay, tempDisplay, feelslikeDisplay, weatherIconDisplay];
+
+    console.log(apiTempResults);
+    console.log(apiResults);
+    console.log(standard);
+    console.log(temp);
+
+    showStandardData(standard, displays);
+})
