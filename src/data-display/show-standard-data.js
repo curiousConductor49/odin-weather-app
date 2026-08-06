8222f027c421ee503d90c312b11e042477b1d698
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-08-06 15:19:15 -0400

    feat: create function to populate dom elements with weather data properties

--- src/data-display/show-standard-data.js
+++ src/data-display/show-standard-data.js
@@ -7,6 +7,15 @@ Body:
 - set the properties of the js object to be the text content of their respective DOM elements (including the img src for the weather icon)
 */
 
-export function showStandardData() {
-    
+export function showStandardData(data, displays) {
+    const { conditions, humidity, windspeed, windgust, temp, feelslike, icon } = data;
+    const [ conditionsDisplay, humidityDisplay, windspeedDisplay, windgustDisplay, tempDisplay, feelslikeDisplay, weatherIconDisplay ] = displays;
+
+    conditionsDisplay.textContent = conditions;
+    humidityDisplay.textContent = `${humidity}%`;
+    windspeedDisplay.textContent = `Wind: ${windspeed} mph`;
+    windgustDisplay.textContent = `Gust: ${windgust} mph`;
+    tempDisplay.textContent = `${temp} F°`;
+    feelslikeDisplay.textContent = `Feels ${feelslike} F°`;
+    weatherIconDisplay.src = `src/assets/icons/${icon}.svg`;
 }
