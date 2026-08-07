9f45819ff47e48716fef05eb607477ced0183fe9
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-08-07 18:39:08 -0400

    chore: remove pseudocode, add comments

--- src/api-data/weather-data.js
+++ src/api-data/weather-data.js
@@ -1,36 +1,10 @@
-/* 
-PSEUDOCODE
-
-class WeatherData
-Params (constructor): user input (text string)
-Properties:
-- weather location (set as user input)
-Methods:
-- async query weather API data
-    - create url request string using weather location
-    - fetch and return API data using request string
-    - catch any errors
-- async query celsius temperature API data
-    - create url request string using weather location (add unitGroup param)
-    - fetch and return API data using request string
-    - catch any errors
-- async process weather API data 
-    - accepts the resolved API data (JSON) as arg
-    - converts the API data into a js obj
-    - create and return a js object w/ only the necessary data
-        - temperature (temp, feelslike)
-        - humidity
-        - wind (windspeed, windgust)
-        - conditions
-        - icon
-*/
-
 export class WeatherData {
     constructor(userInputLocation) {
         this.location = userInputLocation;
         this.apiKey = "7LVJRQVYQG7M8VGRCKHUCV5B3";
     }
 
+    // async methods to fetch API data
     async queryStandardData() {
         try {
             const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${this.location}?key=${this.apiKey}&include=current`);
@@ -64,6 +38,7 @@ export class WeatherData {
         }
     }
 
+    // methods to slim API data into smaller objects
     processStandardData(data) {
         const { conditions, humidity, windspeed, windgust, temp, feelslike, icon } = data.currentConditions;
         const weatherObj = { conditions, humidity, windspeed, windgust, temp, feelslike, icon };
