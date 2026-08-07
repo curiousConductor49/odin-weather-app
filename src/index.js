1673454dd49de1516a7a026e4fba403ca263efcb
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-08-07 18:33:24 -0400

    chore: remove pseudocode, add comments

--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
-/* 
-PSEUDOCODE
-
-top-level:
-module imports (api-data, data-display, user-input)
-
-event listener scope:
-- user btn click -> anon async callback wrapping the bulk of the program
-    - await fetch weather API data, both general and celsius temp (async)
-    - set DOM display visibility from hidden to visible
-    - display weather API data (async)
-    - attach event listener to temp toggle checkbox
-        - checks if checkbox is checked or unchecked
-        - if checked, display celsius temp data
-        - else, display fahrenheit temp data
-    - attach event listener to error msg dialog
-        - close dialog when close btn is clicked
-*/
-
+// module imports
 import { cleanUserInput } from "./user-input/clean-user-input.js";
 import { formatUserInput } from "./user-input/format-user-input.js";
 import { WeatherData } from "./api-data/weather-data.js";
 import { showStandardData } from "./data-display/show-standard-data.js";
 import { showTempData } from "./data-display/show-temp-data.js";
 
-
-// TESTING ZONE
+// DOM elements
 const weatherSearchForm = document.querySelector("#search-form");
 const weatherSearchBar = document.querySelector("#weather-search");
 const errorMsgDialog = document.querySelector("#error-message-dialog");
@@ -41,43 +22,47 @@ const tempDisplay = document.querySelector("#temp-display");
 const feelslikeDisplay = document.querySelector("#feelslike-display");
 const weatherIconDisplay = document.querySelector("#weather-icon-display");
 
+// set DOM weather display visibility to hidden
 window.addEventListener("load", () => weatherDisplay.style.visibility = "hidden");
 
+// form submission calls anon async callback wrapping the bulk of the program
 weatherSearchForm.addEventListener("submit", async (event) => {
     try {
         event.preventDefault();
 
+        // request standard weather API data
         const location = cleanUserInput(event.target.querySelector("#weather-search").value);
         const weather = new WeatherData(location);
         const apiResults = await weather.queryStandardData();
         
+        // cleanly notify user of request errors with a basic modal
         if (apiResults === undefined) {
             errorMsgDialog.showModal();
             return;
         } else {
+            // request temperature API data (celsius, fahrenheit)
             const apiTempCelsiusResults = await weather.queryTempCelsiusData();
             const apiFahrenheitResults = await weather.queryTempFahrenheitData();
 
+            // slim and store API data as smaller objects
             const standard = weather.processStandardData(apiResults);
             const tempCelsius = weather.processTempData(apiTempCelsiusResults);
             const tempFarenheit = weather.processTempData(apiFahrenheitResults);
 
+            // update DOM with API data and user input location
             const displays = [conditionsDisplay, humidityDisplay, windspeedDisplay, windgustDisplay, tempDisplay, feelslikeDisplay, weatherIconDisplay];
-
-            console.log(apiResults);
-            console.log(standard);
-            console.log(tempCelsius);
-            console.log(tempFarenheit);
 
             weatherDisplay.style.visibility = "visible";
             weatherSearchBar.value = "";
-            locationDisplay.textContent = formatUserInput(userInput);
+            locationDisplay.textContent = formatUserInput(location);
             showStandardData(standard, displays);
 
+            // set up toggle to switch between celsius and fahrenheit
             tempUnitToggle.addEventListener("change", (event) => event.target.checked ? showTempData(tempCelsius, displays, true) : showTempData(tempFarenheit, displays, false));
         }
     } catch (error) {
         console.log("Error:", error.message);
     }
 })
+
 closeDialogBtn.addEventListener("click", () => errorMsgDialog.close());
