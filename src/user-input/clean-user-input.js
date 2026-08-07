1972c604899ba7383733a1684b42451feb849acf
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2026-08-07 18:34:47 -0400

    chore: remove pseudocode

--- src/user-input/clean-user-input.js
+++ src/user-input/clean-user-input.js
@@ -1,13 +1,3 @@
-/* 
-PSEUDOCODE:
-
-Function clean user input
-Params: user input val (text string)
-Body:
-- store whitespace-trimmed, lowercased val in variable i.e. clean it
-Return: cleaned user input val
-*/
-
 export function cleanUserInput(input) {
     const cleanInput = input.trim().toLowerCase();
 
--- src/user-input/format-user-input.js
+++ src/user-input/format-user-input.js
@@ -1,17 +1,3 @@
-/* 
-PSEUDOCODE
-
-Function format user input
-Params: user input val (text string)
-Body:
-- split input into an array using spaces for the separator (to handle multi-word location names e.g. "new york" vs "michigan")
-- for each word:
-    - slice, capitalize, and store the first letter
-    - replace the first letter w/ the capitalized version
-- join the words into a space-separated string
-Returns: the user input with capitalized initials
-*/
-
 export function formatUserInput(input) {
     const nameWords = input.split(" ");
     const capitalizedName = nameWords
